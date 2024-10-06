2f1074465defc6fac9c20303774a023b5fa95f47
ilya-skoropad <ilya.skoropad0@gmail.com>
2024-10-06 12:22:23 +0300

    user check for registration

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -11,18 +11,29 @@ type User struct {
 	db *sql.DB
 }
 
-func (u User) GetUser(nickname string) (entity.User, error) {
+func (u User) GetUser(login string) (entity.User, error) {
 	var user entity.User
 	user.Role = &entity.Role{}
 	err := u.db.QueryRow("SELECT U.id, U.login, U.nick, U.password, U.created_at, U.activated_at, R.id, R.name "+
 		"FROM user_api.user AS U JOIN user_api.role R ON U.role = R.id  "+
-		"WHERE nick = $1 LIMIT 1", nickname).
+		"WHERE lower(U.login) = lower($1) LIMIT 1", login).
 		Scan(&user.Id, &user.Login, &user.Nick, &user.Password, &user.CreatedAt, &user.ActivatedAt, &user.Role.Id, &user.Role.Name)
 	if err != nil {
 		return entity.User{}, err
 	}
 
-	return user, err
+	return user, nil
+}
+
+func (u User) CheckUser(login string) (string, error) {
+	var guid string
+
+	err := u.db.QueryRow("SELECT id FROM user_api.user WHERE lower(login) = lower($1) LIMIT 1", login).Scan(&guid)
+	if err != nil {
+		return "", err
+	}
+
+	return guid, nil
 }
 
 func NewUserRepository(env *config.Env) *User {
